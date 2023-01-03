7e7ec425ef7d74a99d534cda1c45d34fe8ab8bfc
Andre Menezes <djdedemenezes@gmail.com>
2023-01-03 17:57:48 -0300

    add feature create category from dashboard

---
+++ app/controllers/dashboard/categories_controller.rb
@@ -0,0 +1,23 @@
+class Dashboard::CategoriesController < ApplicationController
+  layout 'dashboard'
+  def new
+    @category = Category.new
+    authorize [:dashboard, @category]
+  end
+
+  def create
+    @category = Category.new(category_params)
+    authorize [:dashboard, @category]
+    if @category.save
+      redirect_to dashboard_path, notice: 'Category created successfully'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
