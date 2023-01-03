148f32f4ba6906c68dd26d1a6cca04220a92e9df
Andre Menezes <djdedemenezes@gmail.com>
2023-01-03 17:33:14 -0300

    add observatory validations

--- app/models/observatory.rb
+++ app/models/observatory.rb
@@ -1,4 +1,7 @@
 class Observatory < ApplicationRecord
+  OBSERVATORY_TYPES = ['observatory', 'platform', 'fpso']
+  validates :name, :email, :phone_number, :type, presence: true
+  validates :type, inclusion: { in: OBSERVATORY_TYPES}
   has_many :observatory_categories, dependent: :destroy
   has_many :observatory_conflicts, dependent: :destroy
   has_many :observatory_priorities, dependent: :destroy
--- spec/models/observatory_spec.rb
+++ spec/models/observatory_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Observatory, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:email)}
+    it { should validate_presence_of(:phone_number)}
+    it { should validate_presence_of(:type)}
+    it { should validate_inclusion_of(:type).in_array(['observatory', 'platform', 'fpso'])}
+  end
+
   describe 'Associations' do
     it { should have_many(:observatory_categories).dependent(:destroy)}
     it { should have_many(:observatory_priorities).dependent(:destroy)}
