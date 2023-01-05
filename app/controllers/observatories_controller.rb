f3de2e43172d0b0cd556ae8ea59cfb4f6ef90d39
Andre Menezes <djdedemenezes@gmail.com>
2023-01-05 06:19:02 -0300

    add map fixed and card grid

--- app/controllers/observatories_controller.rb
+++ app/controllers/observatories_controller.rb
@@ -1,5 +1,11 @@
 class ObservatoriesController < ApplicationController
   def index
     @observatories = policy_scope(Observatory)
+    @markers = @observatories.geocoded.map do |observatory|
+      {
+        lat: observatory.latitude,
+        lng: observatory.longitude
+      }
+    end
   end
 end
