dad0e4adb58847a1cad03d442ae75ef576660f52
Andre Menezes <djdedemenezes@gmail.com>
2023-01-23 22:12:48 -0300

    fix: before action to admin/dashboard filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_breadcrumbs
-  before_action :authorize_dashboard_user_if_admin if :dashboard_controller?
+  before_action :authorize_dashboard_user_if_admin, if: :dashboard_controller?
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
