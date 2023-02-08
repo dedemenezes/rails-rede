8db34f850241e9e01ea673f312dff113ee34caa7
Andre Menezes <djdedemenezes@gmail.com>
2023-02-08 06:33:52 -0300

    add: swiper margin on start

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -3,8 +3,10 @@ import Swiper from 'swiper';
 
 // Connects to data-controller="swiper"
 export default class extends Controller {
+  static targets = ['wrapper']
+
   connect() {
-    console.log(`Hi, ${this.element}`)
+    console.log(this.wrapperTarget)
     this.swiper = new Swiper(this.element, {
       slidesPerView: 3,
       spaceBetween: 2,
@@ -13,6 +15,12 @@ export default class extends Controller {
         el: ".swiper-pagination",
         clickable: true,
       },
+    })
+    this.swiper.on('slideChange', (event) => {
+      console.log(this.wrapperTarget.classList.contains('container'))
+      if (this.wrapperTarget.classList.contains('container')) {
+        this.wrapperTarget.classList.remove('container')
+      }
     })
   }
 }
