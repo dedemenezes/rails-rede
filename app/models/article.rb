68b6dd75308d14d8d80c30c9d22bf474d638cc97
Andre Menezes <djdedemenezes@gmail.com>
2023-03-05 18:14:36 -0300

    add published to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,8 +9,10 @@ class Article < ApplicationRecord
 
   acts_as_taggable_on :tags
 
+  after_create :verify_featured
+
   def self.dashboard_headers
-    %w[id banner header sub\ header rich\ body]
+    %w[id banner header sub\ header rich\ body featured? published?]
   end
 
   def self.featured
@@ -19,5 +21,16 @@ class Article < ApplicationRecord
 
   def self.has_any_present?
     count.positive?
+  end
+
+  def verify_featured
+    self.featured = Article.featured.nil?
+  end
+
+  def published?
+    published ? '✅' : '❌'
+  end
+  def featured?
+    featured ? '✅' : '❌'
   end
 end
