04ecc8cea313d4a5722ce26e72e351ba2bc3dfee
Andre Menezes <djdedemenezes@gmail.com>
2023-03-05 12:54:42 -0300

    add scopes to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,9 @@ class Article < ApplicationRecord
   has_one_attached :highlight_image
   has_rich_text :rich_body
 
+  scope :main, -> { find_by_featured(true) }
+  scope :all_but_featured, -> { order(created_at: :desc).to_a.delete_if(&:featured) }
+
   def self.dashboard_headers
     %w[id banner header sub\ header rich\ body]
   end
