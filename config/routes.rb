810e6ee1e0e7cbddf375726dbe763343794d1019
Andre Menezes <djdedemenezes@gmail.com>
2023-03-06 04:53:42 -0300

    add inner navigatio nto about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get '/contact', to: 'pages#contact'
+  get '/about_us', to: 'pages#about_us'
   # resources :categories, only: :show
 
   # delete 'attachments/:id', to: 'attachments#destroy', as: :destroy_attachment
