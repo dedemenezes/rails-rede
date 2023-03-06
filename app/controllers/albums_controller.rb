16d36ce2c1350dcf6f700b09082cdea5aa564ba3
Andre Menezes <djdedemenezes@gmail.com>
2023-03-06 03:25:31 -0300

    add breadcrumbs to gallery album flow

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,5 +2,8 @@ class AlbumsController < ApplicationController
   def show
     @album = Album.find(params[:id])
     authorize @album
+    add_breadcrumb 'Galleries', galleries_path
+    add_breadcrumb @album.gallery.name, gallery_path(@album.gallery)
+    add_breadcrumb @album.name, album_path(@album), current: true
   end
 end
--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,10 +1,13 @@
 class GalleriesController < ApplicationController
   def index
     @galleries = policy_scope(Gallery)
+    add_breadcrumb 'Galleries', galleries_path, current: true
   end
 
   def show
     @gallery = Gallery.find(params[:id])
     authorize @gallery
+    add_breadcrumb 'Galleries', galleries_path
+    add_breadcrumb @gallery.name, gallery_path(@gallery), current: true
   end
 end
