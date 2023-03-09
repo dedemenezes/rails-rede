72efb539e608987141e04fd3985297cc9b26d09c
Andre Menezes <djdedemenezes@gmail.com>
2023-03-09 16:30:12 -0300

    add filter to articles

--- app/javascript/controllers/overlay_menu_controller.js
+++ app/javascript/controllers/overlay_menu_controller.js
@@ -2,12 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="overlay-menu"
 export default class extends Controller {
-  static targets = ['menu', 'btn', 'formContainer']
+  static targets = ['menu', 'btn', 'formContainer', 'form', 'input']
   connect() {
   }
 
   toggle() {
     this.formContainerTarget.classList.toggle('active')
+    if (!this.formContainerTarget.classList.includes('active')) {
+      this.inputTarget.focus()
+    }
     // this.formContainerTarget.classList.toggle('overlay__content--active')
   }
 
