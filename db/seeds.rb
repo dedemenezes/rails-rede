04db53864f3a640704ccc8c02559fbe3da113263
Andre Menezes <djdedemenezes@gmail.com>
2023-03-10 10:07:54 -0300

    add observatories to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -59,6 +59,7 @@ description = "O Observatório Araruama é formado por voluntários da comunidad
 Os pescadores artesanais retratam a dificuldade de acesso as politicas públicas em relação à precariedade na infraestrutura para o escoamento do pescado, no qual muitas vezes precisam repassar para atravessadores a preços baixos, desvalorizando o seu trabalho, além da poluição na Laguna de Araruama e o afastamento das suas residências próximas da laguna causadas pelo crescente aumento populacional na região, ocasionado pelo aumento de polos de extração de petróleo na região dos lagos.
 Outro problema relatado é a barreira encontrada pelos pescadores na ocupação de espaços de decisões, onde são debatidos assuntos que podem interferir na prática do seu trabalho como a implantação do transporte hidroviário, Ferry Boat, que atualmente ocupa um espaço considerado da laguna influenciando diretamente na dinâmica da pesca."
 Observatory.create!(
+  published: true,
   name: 'Araruama',
   email: 'pea.araruama@redeobservacao.com',
   phone_number: '21972614293',
@@ -78,6 +79,7 @@ Observatory.create!(
 arraial_desc = "O Observatório de Arraial do Cabo é formado por marisqueiras, beneficiadoras de pescado e pescadores artesanais da Prainha. O grupo enfrenta impactos relacionados à dinâmica demográfica, perda de território e maretório,  conflitos com Unidade de Conservação e vem se organizando e se posicionando na gestão ambiental pública local frente aos conflitos ambientais. Para tanto, o processo educativo e as ações com esse grupo são pautadas no tema gerador definido coletivamente: Viabilização da gestão pesqueira da Reserva Extrativista Marinha (Resex-Mar) de Arraial do Cabo."
 
 arraial = Observatory.create!(
+  published: true,
   name: 'Arraial do Cabo',
   email: 'pea.arraial-do-cabo@redeobservacao.com',
   phone_number: '21972614293',
@@ -97,14 +99,15 @@ arraial = Observatory.create!(
 buzios_desc = "O Observatório de Armação dos Búzios é formado por quilombolas do Quilombo de Baía Formosa. O grupo enfrenta impactos como a especulação imobiliária, descaracterização e perda do território quilombola, dificuldade de acesso a políticas públicas específicas, e vem se organizando e se posicionando na gestão ambiental pública local frente aos conflitos ambientais. Para tanto, o processo educativo e as ações com esse grupo são pautadas no tema gerador definido coletivamente: \"Descaracterização e perda do território quilombola\"."
 
 buzios = Observatory.create!(
+  published: true,
   name: 'Armação dos Búzios',
   email: 'pea.buzios@redeobservacao.com',
   phone_number: '21972614293',
   unity_type: type_observatory,
   neighborhood: 'Quilombo de Baía Formosa',
   city: 'Armação dos Búzios',
-  street: 'Estrada da rasa',
-  number: '0',
+  street: 'Estrada dos Buzios',
+  number: '275',
   zip_code: '28950-000',
   state: 'Rio de Janeiro',
   municipality: 'RJ',
@@ -113,9 +116,11 @@ buzios = Observatory.create!(
   description: buzios_desc
 )
 
+
 campos_desc = "O Observatório de Campos dos Goytacazes é formado por remanescentes quilombolas do Quilombo de Lagoa Fea. O grupo enfrenta impactos como a descaracterização e perda do território quilombola, falta de acesso e existência de políticas públicas, conflitos relacionados à falta de reconhecimento da identidade quilombola por parte da comunidade e do poder público, e vem se organizando e se posicionando na gestão ambiental pública local frente aos conflitos ambientais. Para tanto, o processo educativo e as ações com esse grupo são pautadas no tema gerador definido coletivamente: Reconhecimento da comunidade quilombola para o acesso à políticas públicas"
 
 campos = Observatory.create!(
+  published: true,
   name: 'Campos dos Goytacazes',
   email: 'pea.campos@redeobservacao.com',
   phone_number: '21972614293',
@@ -132,9 +137,152 @@ campos = Observatory.create!(
   description: campos_desc
 )
 
+campos_desc = "O Observatório de Campos dos Goytacazes é formado por remanescentes quilombolas do Quilombo de Lagoa Fea. O grupo enfrenta impactos como a descaracterização e perda do território quilombola, falta de acesso e existência de políticas públicas, conflitos relacionados à falta de reconhecimento da identidade quilombola por parte da comunidade e do poder público, e vem se organizando e se posicionando na gestão ambiental pública local frente aos conflitos ambientais. Para tanto, o processo educativo e as ações com esse grupo são pautadas no tema gerador definido coletivamente: Reconhecimento da comunidade quilombola para o acesso à políticas públicas"
 
+campos = Observatory.create!(
+  published: true,
+  name: 'Campos dos Goytacazes',
+  email: 'pea.campos@redeobservacao.com',
+  phone_number: '21972614293',
+  unity_type: type_observatory,
+  neighborhood: 'Quilombo de Lagoa FEA',
+  city: 'Campos dos Goytacazes',
+  street: 'Quilombo de Lagoa FEA',
+  number: '0',
+  zip_code: '28950-000',
+  state: 'Rio de Janeiro',
+  municipality: 'RJ',
+  priority_type: quilombolas,
+  conflict_type: perda_quilombola,
+  description: campos_desc
+)
 
+cabo_frio_desc = "O Observatório de Cabo Frio é formado por pescadores artesanais de guaiamum do Chavão. O grupo enfrenta impactos como crescimento populacional desordenado, perda de território pesqueiro, especulação imobiliária, falta de acesso e de existência de políticas públicas para manutenção da atividade pesqueira, conflitos com Unidade de Conservação, e vem se organizando e se posicionando na gestão ambiental pública local frente aos conflitos ambientais. Para tanto, o processo educativo e as ações com esse grupo são pautadas no tema gerador definido coletivamente: Manutenção do território pesqueiro."
 
+cabo_frio = Observatory.create!(
+  published: true,
+  name: 'Cabo Frio',
+  email: 'pea.cabo-frio@redeobservacao.com',
+  phone_number: '21972614293',
+  unity_type: type_observatory,
+  neighborhood: 'Chavão',
+  city: 'Cabo Frio',
+  street: 'Chavao, Parque Veneza',
+  number: '0',
+  zip_code: '28925-834',
+  state: 'Rio de Janeiro',
+  municipality: 'RJ',
+  priority_type: pescadores_artesanais,
+  conflict_type: pesqueira,
+  description: cabo_frio_desc
+)
+# CHAVAO =>  22°35'19"S   42°1'22"W
+
+itapemirim_desc = "O Observatório Itapemirim é formado por marisqueiras das localidades de Itaipava e Itaoca. O grupo enfrenta impactos como crescimento populacional, criminalização da atividade tradicional e falta de acesso a políticas públicas. No momento vem entendendo o conflito que está inserido e se organizando em busca de estratégias de enfrentamento. Para tanto, o processo educativo e as ações com esse grupo são pautadas no tema gerador definido coletivamente: Reconhecimento e estruturação da atividade das marisqueiras."
+
+itapemirim = Observatory.create!(
+  published: true,
+  name: 'Itapemirim',
+  email: 'pea.itapemirimo@redeobservacao.com',
+  phone_number: '21972614293',
+  unity_type: type_observatory,
+  neighborhood: 'Itaipava e Itaoca',
+  city: 'Itaoca',
+  street: 'Av. Marataizes',
+  number: '32',
+  zip_code: '29330-000',
+  state: 'Rio de Janeiro',
+  municipality: 'RJ',
+  priority_type: mariqueiras,
+  conflict_type: pesqueira,
+  description: itapemirim_desc
+)
+
+macae_desc = "O Observatório de Macaé tem focado suas ações na mobilização dos agricultores familiares do Imburo e na consolidação de um grupo. Após a conclusão do levantamento dos principais impactos relacionados à cadeia produtiva de petróleo e gás que afetam esses agricultores, será definido coletivamente o tema gerador municipal, que vai pautar o processo educativo com o grupo bem como monitoramento."
+
+macae = Observatory.create!(
+  published: true,
+  name: 'Macaé',
+  email: 'pea.macae@redeobservacao.com',
+  phone_number: '21972614293',
+  unity_type: type_observatory,
+  neighborhood: 'Imburo',
+  city: 'Macaé',
+  street: 'Estrada do Imburo',
+  number: '0',
+  zip_code: '27979-000',
+  state: 'Rio de Janeiro',
+  municipality: 'RJ',
+  priority_type: agricultores,
+  conflict_type: territorio_rural,
+  description: macae_desc
+)
+
+rio_das_ostras_desc = "O Observatório de Rio das Ostras é formado por agricultores familiares de Cantagalo. O grupo enfrenta impactos como crescimento populacional desordenado, especulação imobiliária, dificuldade de acesso a políticas públicas de incentivo à agricultura familiar, implementação e expansão da Zona Especial de Negócios, e vem se organizando e se posicionando na gestão ambiental pública local frente aos conflitos ambientais. Para tanto, o processo educativo e as ações com esse grupo são pautadas no tema gerador definido coletivamente: Acesso a políticas públicas no enfrentamento à descaracterização do território rural."
+
+rio_das_ostras = Observatory.create!(
+  published: true,
+  name: 'Rio das Ostras',
+  email: 'pea.rdostras@redeobservacao.com',
+  phone_number: '21972614293',
+  unity_type: type_observatory,
+  neighborhood: 'Cantagalo',
+  city: 'Rio das Ostras',
+  street: 'Av. Petróleo e Gás',
+  number: '3',
+  zip_code: '28890-000',
+  state: 'Rio de Janeiro',
+  municipality: 'RJ',
+  priority_type: agricultores,
+  conflict_type: territorio_rural,
+  description: rio_das_ostras_desc
+)
+
+itabapoana_desc = "O Observatório São Francisco de Itabapoana é formado por agricultores da comunidade de Carrapato / Nova Belém. O grupo enfrenta impactos como êxodo rural, falta de acesso a políticas públicas de incentivo à agricultura familiar, conflitos com Unidade de Conservação e processos de regularização fundiária, e vem se organizando e se posicionando na gestão ambiental pública local frente aos conflitos ambientais. Para tanto, o processo educativo e as ações com esse grupo são pautadas no tema gerador definido coletivamente: Manutenção da integridade do território agrícola."
+
+itabapoana = Observatory.create!(
+  published: true,
+  name: 'São Francisco de Itabapoana',
+  email: 'pea.itabapoana@redeobservacao.com',
+  phone_number: '21972614293',
+  unity_type: type_observatory,
+  neighborhood: 'Carrapato / Nova Belém',
+  city: 'São Francisco de Itabapoana',
+  street: 'Estrada Barra Seca',
+  number: '3',
+  zip_code: '28230-000',
+  state: 'Rio de Janeiro',
+  municipality: 'RJ',
+  priority_type: agricultores,
+  conflict_type: territorio_rural,
+  description: itabapoana_desc
+)
+
+sao_joao_desc = "O Observatório São Francisco de Itabapoana é formado por agricultores da comunidade de Carrapato / Nova Belém. O grupo enfrenta impactos como êxodo rural, falta de acesso a políticas públicas de incentivo à agricultura familiar, conflitos com Unidade de Conservação e processos de regularização fundiária, e vem se organizando e se posicionando na gestão ambiental pública local frente aos conflitos ambientais. Para tanto, o processo educativo e as ações com esse grupo são pautadas no tema gerador definido coletivamente: Manutenção da integridade do território agrícola."
+
+sjbarra = Observatory.create!(
+  published: true,
+  name: 'São João da Barra',
+  email: 'pea.saojoaodabarra@redeobservacao.com',
+  phone_number: '21972614293',
+  unity_type: type_observatory,
+  neighborhood: 'Atafona',
+  city: 'São João da Barra',
+  street: 'R. Felicíssimo Alves',
+  number: '780',
+  zip_code: '28200-000',
+  state: 'Rio de Janeiro',
+  municipality: 'RJ',
+  priority_type: beneficadoras,
+  conflict_type: pesca_laguna,
+  description: sao_joao_desc
+)
+
+puts 'assigning rich text'
+
+Observatory.all.each do |obs|
+  ActionText::RichText.create!(record_type: 'Observatory', record_id: obs.id, name: 'rich_description', body: obs.description)
+end
 
 puts 'Creating tags...'
 
