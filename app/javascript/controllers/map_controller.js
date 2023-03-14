368a55ffa1c277830ddf611e01268dcfa57440bd
Andre Menezes <djdedemenezes@gmail.com>
2023-03-14 14:12:50 -0300

    not working

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,4 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
+import mapboxgl from 'mapbox-gl'
+// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+import "mapbox-gl/dist/mapbox-gl.css"
+// import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css"
 
 // Connects to data-controller="map"
 export default class extends Controller {
