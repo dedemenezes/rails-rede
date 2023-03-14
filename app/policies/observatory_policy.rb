36933da07c0c72c2bb05a988774870cb0f9111d8
Andre Menezes <djdedemenezes@gmail.com>
2023-03-14 19:59:49 -0300

    removed nplusone query problem when loading info window;

--- app/policies/observatory_policy.rb
+++ app/policies/observatory_policy.rb
@@ -2,7 +2,7 @@ class ObservatoryPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.where(published: true)
+      scope.includes(:conflict_type, :priority_type).where(published: true)
     end
   end
 
