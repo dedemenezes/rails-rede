735db18753a3bc43072e9b5926b414a5cc859a50
Andre Menezes <djdedemenezes@gmail.com>
2023-03-23 09:01:31 -0300

    add scope to filter albums only events

--- app/models/observatory.rb
+++ app/models/observatory.rb
@@ -43,6 +43,11 @@ class Observatory < ApplicationRecord
     ['id', 'name', 'address', 'description', 'published?', 'created at', 'updated_at']
   end
 
+  def events
+    albums.select(&:is_event)
+  end
+
+
   def location
     neighborhood
   end
