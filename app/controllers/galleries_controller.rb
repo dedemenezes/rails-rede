23831748e7e78cd6de853b3e571931627a6fdeca
Andre Menezes <djdedemenezes@gmail.com>
2023-03-23 13:41:17 -0300

    add ordering to galleries, albums and observatories index

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,12 +1,12 @@
 class GalleriesController < ApplicationController
   def index
-    @galleries = policy_scope(Gallery)
+    @galleries = policy_scope(Gallery).order(name: :asc)
     add_breadcrumb 'Acervo', galleries_path, current: true
   end
 
   def show
     @gallery = Gallery.includes(albums: { banner_attachment: :blob}).find_by(name: params[:name]) || Gallery.find(params[:id])
-    @albums = @gallery.published_albums
+    @albums = @gallery.published_albums.sort_by(&:updated_at).reverse
     authorize @gallery
     add_breadcrumb 'Acervo', galleries_path
     add_breadcrumb @gallery.name, gallery_path(@gallery), current: true
