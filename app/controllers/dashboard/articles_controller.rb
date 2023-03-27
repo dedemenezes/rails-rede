b9c37fed01eeebc1ee397b43c46eb6aae063d9bf
Andre Menezes <djdedemenezes@gmail.com>
2023-03-27 15:14:24 -0300

    update controller to change owner of article

--- app/controllers/dashboard/articles_controller.rb
+++ app/controllers/dashboard/articles_controller.rb
@@ -13,14 +13,22 @@ class Dashboard::ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+
+    if params[:article][:project_id].present?
+      @project = Project.find(params[:article][:project_id])
+      @article.project = @project
+    end
+
     if params[:article][:observatory_id].present?
       @observatory = Observatory.find(params[:article][:observatory_id])
       @article.observatory = @observatory
     end
+
     if params[:article][:methodology_id].present?
       @methodology = Methodology.find(params[:article][:methodology_id])
       @article.methodology = @methodology
     end
+
     create_taggings
     if @article.save
       redirect_to dashboard_articles_path
@@ -35,6 +43,9 @@ class Dashboard::ArticlesController < ApplicationController
   def update
     # MUDAR URGENTE
     @article = Article.find_by(header: params[:id])
+    # raise
+    set_article_writer
+
     destroy_taggings
     create_taggings
     if @article.update(article_params)
@@ -50,6 +61,37 @@ class Dashboard::ArticlesController < ApplicationController
   end
 
   private
+
+  def set_article_writer
+    set_project || set_observatory || set_methodology
+  end
+
+  def set_methodology
+    return unless params[:article][:methodology_id].present?
+
+    @article.project = nil
+    @article.observatory = nil
+    @methodology = Methodology.find(params[:article][:methodology_id])
+    @article.methodology = @methodology
+  end
+
+  def set_project
+    return unless params[:article][:project_id].present?
+    binding.break
+    @article.methodology = nil
+    @article.observatory = nil
+    @project = Project.find(params[:article][:project_id])
+    @article.project = @project
+  end
+
+  def set_observatory
+    return unless params[:article][:observatory_id].present?
+
+    @article.methodology = nil
+    @article.observatory = nil
+    @observatory = Observatory.find(params[:article][:observatory_id])
+    @article.observatory = @observatory
+  end
 
   def create_taggings
     return unless params[:article][:tag_ids].present?
@@ -78,6 +120,6 @@ class Dashboard::ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:banner, :header, :sub_header, :rich_body, :featured, :published, :observatory_id)
+    params.require(:article).permit(:banner, :header, :sub_header, :rich_body, :featured, :published, :observatory_id, :methodology_id, :project_id)
   end
 end
