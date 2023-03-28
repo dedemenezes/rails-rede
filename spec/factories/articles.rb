83befc558e8f15900446807787b4ff85fbf59ffc
Andre Menezes <djdedemenezes@gmail.com>
2023-03-27 21:45:02 -0300

    add project and articles to factory, serious ones

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,17 @@
 FactoryBot.define do
   factory :article do
-    header { "MyString" }
-    sub_header { "MyString" }
+    header { "This is a very nice header for this article" }
+    sub_header { "This is what we meant with a sub header" }
+    featured { true }
+    published { true }
+    association :project, factory: :rede
+    rich_body {
+      ActionText::RichText.create(
+        { record_type: 'Article',
+          name: "content",
+          body: "<div class='trix-content'>HELLO</div>",
+          record_id: self.id }
+      )
+    }
   end
 end
