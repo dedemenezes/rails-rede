6f0e1e262910cb77965a69c8e74ac2fb47f984be
Andre Menezes <djdedemenezes@gmail.com>
2023-03-29 21:03:12 -0300

    add simple sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,45 +1,48 @@
-SitemapGenerator::Sitemap.default_host = 'https:/www.pearedeobservacao.com'
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://www.pearedeobservacao.com"
+
 SitemapGenerator::Sitemap.create do
-  add '/observatorios'
-  add '/contact_us'
-  add '/contato'
-  add '/sobre'
-  add '/mapa-de-conflitos'
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+
+  add '/contato', priority: 0.9
+  add '/sobre', priority: 0.9
+  add '/mapa-de-conflitos', priority: 0.9
+  #
+  # Add '/articles'
+  add articles_path, :priority => 0.5, :changefreq => 'daily'
+
+  # Add all articles:
+  Article.find_each do |article|
+    add article_path(article), :lastmod => article.updated_at, priority: 0.5
+  end
+
+  # Add '/observatories'
+  add observatories_path, priority: 0.7
+  # Add all observatories:
+  Observatory.find_each do |observatory|
+    add observatory_path(observatory), lastmod: observatory.updated_at, priority: 0.6
+  end
+
+  # Add '/galleries'
+  add galleries_path, priority: 0.8
+  # Add all galleries:
+  Gallery.find_each do |gallery|
+    add gallery_path(gallery), lastmod: gallery.updated_at, priority: 0.7
+  end
+
+  # Add all albums:
+  Album.find_each do |album|
+    add album_path(album), lastmod: album.updated_at, priority: 0.7
+  end
 end
-# SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
-
-
-
-
-
-# blog_sitemap_opts = {
-#   create_index: false,
-#   default_host: 'https://fastruby.io/blog',
-#   compress: false,
-#   sitemaps_path: '',
-#   namer: SitemapGenerator::SimpleNamer.new(:blog_sitemap)
-# }
-
-# SitemapGenerator::Sitemap.create blog_sitemap_opts do
-#   pages = Dir["public/blog/**/*.html"]
-#   pages.each do |blog_page|
-#     add blog_page, changefreq: 'weekly'
-#   end
-# end
-
-# sitemap_opts = {
-#   create_index: false,
-#   default_host: 'https://fastruby.io',
-#   compress: false,
-#   sitemaps_path: '',
-#   namer: SitemapGenerator::SimpleNamer.new(:sitemap)
-# }
-
-# SitemapGenerator::Sitemap.create sitemap_opts do
-#   add '/#contact-us', changefreq: 'weekly'
-#   add '/team', changefreq: 'weekly'
-
-#   # all other important pages here
-
-#   add_to_index "blog_sitemap.xml", host: ENV['SITE_URL']
-# end
