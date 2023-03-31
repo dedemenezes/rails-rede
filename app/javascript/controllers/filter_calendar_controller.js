5ada93d885c93e5d5e4c77d748197e400b114480
Andre Menezes <djdedemenezes@gmail.com>
2023-03-31 08:37:53 -0300

    refac button click to open calendar filter on the home page

--- app/javascript/controllers/filter_calendar_controller.js
+++ app/javascript/controllers/filter_calendar_controller.js
@@ -4,20 +4,24 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ['input', 'form', 'results']
   connect() {
+    console.log(this.inputTarget._flatpickr);
+
   }
 
-  formDisplay() {
-    this.formTarget.classList.remove('d-none')
+  formToggle() {
+    this.formTarget.classList.toggle('d-none')
     setTimeout(() => {
-      this.formTarget.classList.remove('box--hidden')
-      this.inputTarget._flatpickr.open()
+      this.formTarget.classList.toggle('box--hidden')
+      if (this.formTarget.classList.contains('d-none')) {
+        this.inputTarget._flatpickr.close()
+      } else {
+        this.inputTarget._flatpickr.open()
+      }
     }, 150)
   }
 
   search(e) {
     e.preventDefault()
-    console.log(this.inputTarget);
-    console.log();
     this.url = `${this.formTarget.action}?before_date=${this.inputTarget.value}`
     fetch(this.url, {
       headers: { 'Accept': 'text/plain' }
