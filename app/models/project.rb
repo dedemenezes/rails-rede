2703acf2fd5244a4d2c10e33dd1609bc9cbbc596
Andre Menezes <djdedemenezes@gmail.com>
2023-06-22 16:35:30 -0300

    add callback to strip video id from url provided by user

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,6 +8,7 @@ class Project < ApplicationRecord
   has_one_attached :slide_three
   has_rich_text :content
   validates :banner_text, presence: true, length: { minimum: 53, maximum: 100 }
+  before_validation :strip_video_id
 
   def self.dashboard_headers
     attribute_names.reject { |a| ["slide_one_subtitle", "slide_two_subtitle", "slide_three_subtitle"].include?(a) }.insert(1, 'banner')
@@ -16,5 +17,12 @@ class Project < ApplicationRecord
 
   def to_s
     'projects'
+  end
+
+  def strip_video_id
+    return unless video_id.present?
+
+    id = video_id.match /watch\?v=(?<id>.*)&+.*\z/
+    self.video_id = id[:id]
   end
 end
