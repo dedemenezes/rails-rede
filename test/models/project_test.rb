35b1c9feb5b561a948527936214467d429ec9572
Andre Menezes <djdedemenezes@gmail.com>
2023-07-13 19:20:21 -0300

    add test for strip youtube video id from youtube links

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#strip_video_id assign correct video_id attrbibute from YouTube full URL' do
+    project = Project.new video_id: 'https://www.youtube.com/watch?v=ccuc6dgTec0'
+    project.strip_video_id
+    assert_equal project.video_id, "ccuc6dgTec0"
+  end
+
+  test '#strip_video_id assign correct video_id attrbibute from YouTube shortned URL' do
+    project = Project.new video_id: 'https://youtu.be/ccuc6dgTec0'
+    project.strip_video_id
+    assert_equal project.video_id, "ccuc6dgTec0"
+  end
 end
