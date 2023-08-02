5688b4bab6e188b232e745eaec0eb43d7363e8b9
Andre Menezes <djdedemenezes@gmail.com>
2023-08-02 20:24:06 -0300

    fix breadcrumb backlink to gallery

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,8 +3,11 @@ class AlbumsController < ApplicationController
     @album = Album.includes(photos_attachments: :blob).find_by(name: params[:name])
     @documents = @album.documents
     authorize @album
+
+    gallery_breadcrumb_type_info = @album.documents.attached? ? '(Documentos)' : '(Imagens)'
+    path_to_gallery = @album.documents.attached? ? documentos_gallery_path(@album.gallery) : imagens_gallery_path(@album.gallery)
     add_breadcrumb 'Acervo', galleries_path
-    add_breadcrumb @album.gallery.name, gallery_path(@album.gallery)
+    add_breadcrumb "#{@album.gallery.name} #{gallery_breadcrumb_type_info}", path_to_gallery
     add_breadcrumb @album.name, album_path(@album), current: true
   end
 end
