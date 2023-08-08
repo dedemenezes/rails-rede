cc6078efdc7ef03b160e3ff2567920219ce93f98
Andre Menezes <djdedemenezes@gmail.com>
2023-08-08 16:20:11 -0300

    fix publicar input on gallery and add stimulus controller to update inner text

---
+++ app/javascript/controllers/publish_checkbox_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="publish-checkbox"
+export default class extends Controller {
+  static targets = ['label', 'input']
+  connect() {
+    this.updateHint()
+  }
+
+  updateHint(event = null) {
+    if (this.inputTarget.checked) {
+      this.labelTarget.innerText = "Publicar!"
+    } else {
+      this.labelTarget.innerText = "Publicar?"
+    }
+  }
+}
