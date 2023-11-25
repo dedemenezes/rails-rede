fad895057ef7a9273df0a62d65439c667c8a2e04
Andre Menezes <djdedemenezes@gmail.com>
2023-11-25 17:10:52 -0300

    add tileset to mapbox using SDKRubyAWS service

--- app/services/s_d_k_ruby_a_w_s.rb
+++ app/services/s_d_k_ruby_a_w_s.rb
@@ -17,10 +17,25 @@ class SDKRubyAWS
 
   def stage_into_bucket
     retrieve_s3_credentials
-    upload_file
+    stage_file
+    upload_tileset
   end
 
   private
+
+  def upload_tileset
+    # curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
+    #   "url": "http://{bucket}.s3.amazonaws.com/{key}",
+    #   "tileset": "dedemenezes.traverse-bay",
+    #   "name": "Traverse Bay"
+    # }' 'https://api.mapbox.com/uploads/v1/dedemenezes?access_token=YOUR_MAPBOX_ACCESS_TOKEN'
+    connection = Faraday.new(headers: { 'Content-Type' => 'application/json', "Cache-Control" => "no-cache" })
+
+    tile_set_body = JSON.stringify({ "url" => body['url'], "tileset" => "dedemenezes.traverse-bay--demo", "name" => "Traverse Bay Demo 01" })
+    binding.b
+    response = connection.post url, body
+    p response
+  end
 
   def retrieve_s3_credentials
     connection = Faraday.new(headers: { 'Content-Type' => 'application/json' })
@@ -31,9 +46,11 @@ class SDKRubyAWS
       { 'Content-Type' => 'application/json' }
     )
     @body = JSON.parse(response.body)
+    binding.b
+
   end
 
-  def upload_file
+  def stage_file
     # aws_access_key_id, aws_secret_access_key
     # sessionToken
     credentials = Aws::Credentials.new(body['accessKeyId'], body['secretAccessKey'], body['sessionToken'])
@@ -41,7 +58,9 @@ class SDKRubyAWS
     object = Aws::S3::Object.new(body['bucket'], body['key'], nil, client:)
     file_path = '/mnt/c/Users/matme/Downloads/Araruama1.kml'
     response = object.upload_file(file_path)
-    puts "File Uploaded! zo/\nObject: #{response}"
+    binding.b
+
+    puts "File Uploaded! zo/\nResponse: #{response}"
   rescue Aws::Errors::ServiceError => e
     puts "Couldn't upload file #{file_path} to #{object.key}. Here's why: #{e.message}"
   end
