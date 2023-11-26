d0dcbfe87c7baca49c670056efcdb760d1349879
Andre Menezes <djdedemenezes@gmail.com>
2023-11-26 19:53:30 -0300

    add new tileset to mapbox from dashboard totaly functional

--- app/controllers/dashboard/mapbox/tilesets_controller.rb
+++ app/controllers/dashboard/mapbox/tilesets_controller.rb
@@ -1,9 +1,13 @@
 class Dashboard::Mapbox::TilesetsController < DashboardController
   def create
-    binding.b
-    response = MapboxUploader.tileset_from_kml(params[:name], params[:tileset].to_path)
+    begin
+      response = MapboxUploader.tileset_from_kml(params[:name], params[:tileset].to_path)
+    rescue S3::NotAuthorizedError => e
+      response = e
+    end
+
     respond_to do |format|
-      format.json { render json: response.body }
+      format.json { render json: response.body, status: response.status }
     end
   end
 end
