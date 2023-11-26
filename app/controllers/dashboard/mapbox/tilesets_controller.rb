ec8247b91e37229140fc693174ca8e2b1072695c
Andre Menezes <djdedemenezes@gmail.com>
2023-11-26 17:31:36 -0300

    add parameters to mapboxuploader to make it reusable]

--- app/controllers/dashboard/mapbox/tilesets_controller.rb
+++ app/controllers/dashboard/mapbox/tilesets_controller.rb
@@ -1,7 +1,7 @@
 class Dashboard::Mapbox::TilesetsController < DashboardController
   def create
     binding.b
-    response = MapboxUploader.tileset_from_kml(params[:tileset].to_path)
+    response = MapboxUploader.tileset_from_kml(params[:name], params[:tileset].to_path)
     respond_to do |format|
       format.json { render json: response.body }
     end
--- app/services/mapbox_uploader.rb
+++ app/services/mapbox_uploader.rb
@@ -3,14 +3,17 @@ require "aws-sdk-s3"
 class MapboxUploader
   attr_reader :body
 
-  def initialize(file_path:)
+  USERNAME = 'dedemenezes'
+
+  def initialize(tileset_name:, file_path:)
     @file_path = file_path
+    @tileset_name = ensure_valid_name(tileset_name)
     @upload_url      = "https://api.mapbox.com/uploads/v1/dedemenezes?access_token=#{ENV.fetch('MAPBOX_SUPER_KEY')}"
     @credentials_url = "https://api.mapbox.com/uploads/v1/dedemenezes/credentials?access_token=#{ENV.fetch('MAPBOX_SUPER_KEY')}"
   end
 
-  def self.tileset_from_kml(file_path)
-    sdk_ruby = new(file_path: file_path)
+  def self.tileset_from_kml(tileset_name, file_path)
+    sdk_ruby = new(tileset_name: tileset_name, file_path: file_path)
     sdk_ruby.tileset_from_kml
   end
 
@@ -21,6 +24,10 @@ class MapboxUploader
   end
 
   private
+
+  def ensure_valid_name(tileset_name)
+    tileset_name.downcase.gsub(' ', '_')
+  end
 
   def retrieve_s3_credentials
     connection = Faraday.new(headers: { 'Content-Type' => 'application/json' })
@@ -46,7 +53,14 @@ class MapboxUploader
 
   def upload_tileset
     connection = Faraday.new(headers: { 'Content-Type' => 'application/json', "Cache-Control" => "no-cache" })
-    tile_set_body = JSON.generate({ "url" => body['url'], "tileset" => "dedemenezes.traverse-bay--demo", "name" => "Traverse Bay Demo 01" })
+    tile_set_body = JSON.generate(
+      {
+        "url" => body['url'],
+        "tileset" => "#{USERNAME}.#{@tileset_name}",
+        "name" => @tileset_name
+      }
+    )
+    binding.b
     connection.post @upload_url, tile_set_body
   end
 end
