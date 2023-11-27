7c679b105d4143ebd7047f254a10f763c7fef281
Andre Menezes <djdedemenezes@gmail.com>
2023-11-27 03:28:38 -0300

    add tileset info to view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,12 @@ class DashboardController < ApplicationController
     @project_count = Project.count
     @methodology_count = Methodology.count
     @user_count = User.count
+    @tilesets = Tileset.all.map do |tileset|
+      {
+        sourceValue: tileset.mapbox_id,
+        urlValue: "mapbox://dedemenezes.#{tileset.mapbox_id}"
+      }
+    end
     authorize [:dashboard, current_user]
   end
 end
