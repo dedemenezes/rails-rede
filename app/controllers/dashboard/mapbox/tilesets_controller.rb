6b6fea476ae29b8e83d930326d499e685994ad8d
Andre Menezes <djdedemenezes@gmail.com>
2023-11-26 21:47:43 -0300

    refac and fix redirect and flash

--- app/controllers/dashboard/mapbox/tilesets_controller.rb
+++ app/controllers/dashboard/mapbox/tilesets_controller.rb
@@ -8,25 +8,36 @@ class Dashboard::Mapbox::TilesetsController < DashboardController
     @dashboard_tileset = Tileset.new(tileset_params)
     respond_to do |format|
       if @dashboard_tileset.save
-        @dashboard_tileset.kml.blob.filename = ActiveStorage::Filename.new(Tileset.new
-                                                      .replace_non_ascii_with_ascii(
-                                                        @dashboard_tileset.kml.blob.filename.to_s.downcase
-                                                      ))
-        @dashboard_tileset.kml.blob.save
-        begin
-          response = MapboxUploader.tileset_from_kml(@dashboard_tileset.mapbox_id, tileset_params[:kml].path)
-        rescue S3::NotAuthorizedError => e
-          response = e
+        rename_kml_blob
+        upload_tileset_to_mapbox
+        format.html do
+          flash[:notice] = 'Tileset created!'
+          redirect_to dashboard_mapbox_tilesets_path
         end
-        format.html { redirect_to dashboard_mapbox_tilesets_path, flash[:notice] = 'Tileset created!' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
-      format.json { render json: response.body, status: response.status }
+      format.json { render json: @response.body, status: @response.status }
     end
   end
 
   private
+
+  def upload_tileset_to_mapbox
+    begin
+      @response = MapboxUploader.tileset_from_kml(@dashboard_tileset.mapbox_id, tileset_params[:kml].path)
+    rescue S3::NotAuthorizedError => e
+      @response = e
+    end
+  end
+
+  def rename_kml_blob
+    @dashboard_tileset.kml.blob.filename = ActiveStorage::Filename.new(Tileset.new
+                                              .replace_non_ascii_with_ascii(
+                                                @dashboard_tileset.kml.blob.filename.to_s.downcase
+                                              ))
+    @dashboard_tileset.kml.blob.save
+  end
 
   def tileset_params
     params.require(:tileset).permit(:name, :kml)
