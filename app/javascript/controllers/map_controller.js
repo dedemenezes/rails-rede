f832f7c88fe70319d71458400e6ab15cc718082a
Andre Menezes <djdedemenezes@gmail.com>
2023-11-29 23:09:54 -0300

    wip - adding name layers

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -32,22 +32,71 @@ export default class extends Controller {
     this.#addNavigationtoMap()
     this.#addListenersToMeniuOptions()
 
-    this.map.on('load', () => {
+    this.map.on('load', (e) => {
+      console.log(e)
       this.tilesetsValue.forEach((tileset) => {
+        // Log the features to the console
+        const features = this.map.querySourceFeatures(tileset.sourceValue);
+        console.log(features);
+
         // Add Araruama tileset as source
         this.addSource(tileset)
 
         // Add new layer to dislay the tileset
         this.addSourceLayer(tileset)
 
+        // Add new layer to dislay the tileset as points
+        this.addPointLayer(tileset)
+
         this.addSourcePopupsOnHovering(tileset)
       })
     })
 
+    // this.map.on('load', () => {
+    //   // Load an image from an external URL.
+    //   this.map.loadImage(
+    //     'https://docs.mapbox.com/mapbox-gl-js/assets/cat.png',
+    //     (error, image) => {
+    //       if (error) throw error;
+
+    //       // Add the image to the map style.
+    //       this.map.addImage('cat', image);
+
+    //       // Add a data source containing one point feature.
+    //       this.map.addSource('point', {
+    //         'type': 'geojson',
+    //         'data': {
+    //           'type': 'FeatureCollection',
+    //           'features': [
+    //             {
+    //               'type': 'Feature',
+    //               'geometry': {
+    //                 'type': 'Point',
+    //                 'coordinates': [-77.4144, 25.0759]
+    //               }
+    //             }
+    //           ]
+    //         }
+    //       });
+
+    //       // Add a layer to use the image to represent the data.
+    //       this.map.addLayer({
+    //         'id': 'points',
+    //         'type': 'symbol',
+    //         'source': 'point', // reference the data source
+    //         'layout': {
+    //           'icon-image': 'cat', // reference the image
+    //           'icon-size': 0.25
+    //         }
+    //       });
+    //     }
+    //   );
+    // });
+
   }
 
   addSource(tileset) {
-    console.log(tileset);
+    // console.log(tileset);
     this.map.addSource(tileset.sourceValue, {
       type: 'vector',
       url: tileset.urlValue,
@@ -57,7 +106,7 @@ export default class extends Controller {
 
   addSourceLayer(tileset) {
     this.map.addLayer({
-      'id': tileset.sourceValue,
+      'id': tileset.sourceValue + '-fill',
       'type': 'fill',
       'source': tileset.sourceValue,
       'layout': {
@@ -65,10 +114,62 @@ export default class extends Controller {
         'visibility': 'visible'
       },
       'paint': {
-        'fill-color': 'rgba(55,148,179,1)'
+        'fill-color': 'rgba(55,148,179,0.5)'
       },
-      'source-layer': tileset.sourceValue
+      'source-layer': tileset.sourceValue,
+      'filter': ['==', ['geometry-type'], 'Polygon']
     })
+    // Add a symbol layer for displaying names on fill polygons
+    this.map.addLayer({
+      'id': tileset.sourceValue + '-fill-label',
+      'type': 'symbol',
+      'source': tileset.sourceValue,
+      'layout': {
+        'text-field': ['get', 'name'], // Display the 'name' property as text
+        'text-size': 12,
+        'text-anchor': 'bottom'
+      },
+      'paint': {
+        'text-color': 'rgba(0,0,0,1)'
+      },
+      'source-layer': tileset.sourceValue,
+      'filter': ['==', ['geometry-type'], 'Polygon'] // Filter for polygon features
+    });
+  }
+
+  addPointLayer(tileset) {
+    this.map.addLayer({
+      'id': tileset.sourceValue + '-point',
+      'type': 'circle', // Use 'circle' for point data
+      'source': tileset.sourceValue,
+      'layout': {
+        // Make the layer visible by default.
+        'visibility': 'visible',
+        // 'text-field': ['get', 'name'], // Display the 'name' property as text
+        // 'text-size': 12
+      },
+      'paint': {
+        'circle-radius': 2,
+        'circle-color': 'rgba(255,0,0,1)'
+      },
+      'source-layer': tileset.sourceValue,
+      'filter': ['==', ['geometry-type'], 'Point']
+    });
+    this.map.addLayer({
+      'id': tileset.sourceValue + '-point-label',
+      'type': 'symbol',
+      'source': tileset.sourceValue,
+      'layout': {
+        'text-field': ['get', 'name'], // Display the 'name' property as text
+        'text-size': 12,
+        'text-anchor': 'bottom'
+      },
+      'paint': {
+        'text-color': 'rgba(0,0,0,1)'
+      },
+      'source-layer': tileset.sourceValue,
+      'filter': ['==', ['geometry-type'], 'Point'] // Filter for point features
+    });
   }
 
   addSourcePopupsOnHovering(tileset) {
@@ -91,7 +192,7 @@ export default class extends Controller {
     // }
     // })
 
-    this.map.on('mouseenter', tileset.sourceValue, (e) => {
+    this.map.on('mouseenter', tileset.sourceValue + '-fill', (e) => {
       this.addSourcePopup(e)
       console.log(`ENTROU!`);
       // Single out the first found feature.
@@ -99,7 +200,7 @@ export default class extends Controller {
       console.log(e);
     })
 
-    this.map.on('mouseleave', tileset.sourceValue, (e) => {
+    this.map.on('mouseleave', tileset.sourceValue + '-fill', (e) => {
       console.log(`SAIU!`);
       console.log(e);
       this.removeSourcePopup()
@@ -127,13 +228,13 @@ export default class extends Controller {
     let popHTML = `<p>`
     const properties = event.features[0].properties
     // console.log(properties);
-    Object.entries(properties).forEach((k) => popHTML += `<strong>${k[0]}:</strong> ${k[1]}`)
+    Object.entries(properties).forEach((k) => popHTML += `<strong>${k[0]}:</strong> ${k[1]}<br>`)
     popHTML += '</p>'
 
     this.popup = new mapboxgl.Popup().setHTML(popHTML)
     // Display a popup with the name of the county
     this.popup.setLngLat([event.lngLat.lng, event.lngLat.lat])
-              .addTo(this. map);
+              .addTo(this.map);
       // .setText(feature.properties.Description)
   }
 
