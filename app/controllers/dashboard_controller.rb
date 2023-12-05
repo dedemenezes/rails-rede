f0f572f8033747491cf00f6a32483a03632c9080
Andre Menezes <djdedemenezes@gmail.com>
2023-12-05 19:18:53 -0300

    add icon to tilesets json into HTML

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,10 +6,16 @@ class DashboardController < ApplicationController
     @methodology_count = Methodology.count
     @user_count = User.count
     @tilesets = Tileset.all.map do |tileset|
+      geo_json = JSON.parse(tileset.geo_json)
+      features = geo_json['features']
+      points = features.select { |f| f['geometry']['type'] == 'Point' }
+      icons = points.uniq { |f| f['properties']['icon'] }
+                    .map { |f| f['properties']['icon'] }
       {
         sourceValue: tileset.mapbox_id,
         urlValue: "mapbox://dedemenezes.#{tileset.mapbox_id}",
-        geoJson: tileset.geo_json
+        geoJson: tileset.geo_json,
+        icons:
       }
     end
     authorize [:dashboard, current_user]
