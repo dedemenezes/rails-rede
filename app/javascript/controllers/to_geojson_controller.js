92b87675c77e7aec175d5ff5ee5b8eede0c6ad46
Andre Menezes <djdedemenezes@gmail.com>
2024-01-29 20:06:59 -0300

    removed console.logs

--- app/javascript/controllers/to_geojson_controller.js
+++ app/javascript/controllers/to_geojson_controller.js
@@ -7,7 +7,6 @@ export default class extends Controller {
   static targets = ['labelGeoJson', 'inputFile', 'inputGeoJson', 'preTag']
 
   connect() {
-    // console.log(this)
 
   }
 
@@ -17,13 +16,11 @@ export default class extends Controller {
   convert(event) {
     const file = this.inputFileTarget.files[0]
     if(file) {
-      // console.log(file);
       // const fr = new FileReader()
       fetch(this.objectURL(file))
         .then(response => response.blob())
         .then((blob) => {
           // const kml = new File([blob], file.name, { type: "kml" })
-          // console.log(kml)
           const reader = new FileReader()
           reader.onload = (event) => {
             this.#parseKmlToGeoJson(event)
@@ -33,8 +30,6 @@ export default class extends Controller {
           // this.
         })
     } else {
-      // console.log(event.currentTarget);
-      // console.log(event.currentTarget.files);
     }
   }
 
@@ -44,27 +39,18 @@ export default class extends Controller {
   };
 
   #parseKmlToGeoJson(event) {
-    // console.log(this)
-    // console.log(this.inputGeoJsonTarget)
 
     const kmlContent = event.target.result
     const kml = new DOMParser().parseFromString(kmlContent, 'text/xml');
-    // console.log(kml)
     if (kml.documentElement.nodeName === 'parsererror') {
-      console.error('Erro ao analisar o documento XML.');
       return;
     }
-    // console.log(kmlContent)
     const convertedWithStyles = toGeoJSON.kml(kml, { styles: true });
-    console.log(convertedWithStyles)
     convertedWithStyles.features.filter(feature => feature.geometry.type === 'Point').forEach(feature => feature.properties.icon = `https${feature.properties.icon.substring(4)}`)
-    // console.log("AFTER")
-    // console.log(convertedWithStyles)
 
     kml.getElementsByTagName('MultiGeometry')[0].childNodes.forEach((geometryNode) => {
 
       // featureProperties = Object.assign({}, convertedWithStyles.features[featureIndex].properties)
-      // console.log(featureProperties)
       if (geometryNode.nodeType === 1) { // Check if it's an element node
         if (geometryNode.nodeName === 'MultiGeometry') {
           // Handle nested MultiGeometry, iterate through its child nodes
@@ -85,13 +71,10 @@ export default class extends Controller {
               }
 
               const placemark = geometryNode.parentElement.parentElement
-              console.dir(placemark)
               // 1. NAME
               const name = placemark.querySelector('name')
               if (name) {
                 const nameText = name.textContent
-                console.log(nameText)
-                console.log(convertedWithStyles.features)
                 const parentFeature = convertedWithStyles.features.find((element) => {
                   if (element.properties && element.properties.name) {
                     return element.properties['name'] === nameText
@@ -102,14 +85,12 @@ export default class extends Controller {
                 }
               }
 
-              console.log(feature)
               convertedWithStyles.features.push(feature);
             }
           })
         }
       }
     })
-    console.log(convertedWithStyles)
 
     this.inputGeoJsonTarget.innerText = JSON.stringify(convertedWithStyles)
     // this.adjustTextareaHeight(this.inputGeoJsonTarget)
