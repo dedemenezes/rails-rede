54ebbce698acd1873aaa68f28e2a2493d8b8dd0e
Andre Menezes <djdedemenezes@gmail.com>
2024-03-11 15:23:20 -0300

    refac MapSubtitlesHelper to be used without namespace

--- app/helpers/map_subtitles_helper.rb
+++ app/helpers/map_subtitles_helper.rb
@@ -1,30 +1,42 @@
 module MapSubtitlesHelper
-  def self.icons_and_texts
+  POLYGONS = {
+    'icon--dark-blue': 'Território do grupo tradicional',
+    'icon--green': 'Unidade de Conservação Terrestre',
+    'icon--light-blue': 'Unidade de Conservação Marinha',
+    'icon--pink': 'Área de moradia',
+    'icon--red': 'Área de Conflito'
+  }.freeze
+
+  PINS = {
+    'ylw-pushpin': 'Observatório e instituições',
+    'blue-pushpin': 'Práticas laborais e sociais',
+    'red-pushpin': 'Estruturas que interferem na atividade social',
+    'pink-pushpin': 'Locais de serviço público',
+    'wht-pushpin': 'Outros pontos de referências'
+  }.freeze
+
+  LINES = {
+    black: 'Dutos',
+    blue: 'Rios',
+    pink: 'Ruas'
+  }.freeze
+
+  ICONS = {
+    water: 'Lagoa',
+    'wht-blank': 'Locais de serviços públicos',
+    'blu-blank': 'Pontos importantes para práticas laborais'
+  }.freeze
+
+  def icons_and_texts
     {
-      polygons: {
-        'icon--dark-blue': 'Território do grupo tradicional',
-        'icon--green': 'Unidade de Conservação Terrestre',
-        'icon--light-blue': 'Unidade de Conservação Marinha',
-        'icon--pink': 'Área de moradia',
-        'icon--red': 'Área de Conflito'
-      },
-      pins: {
-        'ylw-pushpin': 'Observatório e instituições',
-        'blue-pushpin': 'Práticas laborais e sociais',
-        'red-pushpin': 'Estruturas que interferem na atividade social',
-        'pink-pushpin': 'Locais de serviço público',
-        'wht-pushpin': 'Outros pontos de referências'
-      },
-      lines: {
-        black: 'Dutos',
-        blue: 'Rios',
-        pink: 'Ruas'
-      },
-      icons: {
-        'wht-blank': 'Locais de serviços públicos',
-        'blu-blank': 'Pontos importantes para práticas laborais',
-        'water': 'Lagoa'
-      }
+      polygons: POLYGONS,
+      pins: PINS,
+      lines: LINES,
+      icons: ICONS
     }
+  end
+
+  def water?(icon)
+    icon == :water ? 'shapes' : 'paddle'
   end
 end
