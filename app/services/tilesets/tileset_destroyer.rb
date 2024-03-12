bf76e35ffd52af75cd181a6c81e1e267cdc7a247
Andre Menezes <djdedemenezes@gmail.com>
2024-03-12 15:44:42 -0300

    add methdo to delete source when deleting tileset

--- app/services/tilesets/tileset_destroyer.rb
+++ app/services/tilesets/tileset_destroyer.rb
@@ -7,6 +7,15 @@ module Tilesets
     end
 
     def run
+      source_response = delete_source
+      return unless source_response.status == 204
+
+      delete_tileset
+    end
+
+    private
+
+    def delete_tileset
       url = "/tilesets/v1/#{tileset_id}"
       conn = Faraday.new(url: "https://api.mapbox.com") do |faraday|
         faraday.adapter Faraday.default_adapter
@@ -14,5 +23,14 @@ module Tilesets
       conn.delete { |req| req.url "#{url}?access_token=#{ENV.fetch("MAPBOX_SUPER_KEY")}" }
     end
 
+    def delete_source
+      username, id = tileset_id.split('.')
+      url  = "tilesets/v1/sources/#{username}/#{id}"
+
+      conn = Faraday.new(url: "https://api.mapbox.com") do |faraday|
+        faraday.adapter Faraday.default_adapter
+      end
+      conn.delete { |req| req.url "#{url}?access_token=#{ENV.fetch("MAPBOX_SUPER_KEY")}" }
+    end
   end
 end
