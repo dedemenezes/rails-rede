333fd909de9c99f50bb5b7eb33cc7f50f584b1a0
Andre Menezes <djdedemenezes@gmail.com>
2024-05-23 18:35:42 -0300

    remove console log

--- app/javascript/controllers/toggle_on_change_controller.js
+++ app/javascript/controllers/toggle_on_change_controller.js
@@ -4,12 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ['category', 'video', 'photo', 'document']
   connect() {
-    const elements = ['video', 'photo', 'document'].map(e => this[`${e}Target`])
-    console.log(elements);
-
-    console.log(this.categoryTarget);
-    console.log(this.categoryTarget.value);
-    console.log(this[`${this.categoryTarget.value}Target`]);
     this.dynamicTarget(this.categoryTarget.value).classList.remove('d-none')
   }
 
