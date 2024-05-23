66a54f352988709319394747092542f2acaf7f99
Andre Menezes <djdedemenezes@gmail.com>
2024-05-23 17:02:15 -0300

    add controller action and refactor existing to use category

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -7,43 +7,50 @@ class GalleriesController < ApplicationController
   def show
     @gallery = Gallery.includes(albums: [:documents_attachments,
                                          { banner_attachment: :blob }]).find_by(name: params[:name]) || Gallery.find(params[:id])
-    @albums = @gallery.published_albums.sort_by(&:updated_at).reverse
+    # @albums = @gallery.published_albums.sort_by(&:updated_at).reverse
     if params[:t].present?
-      if params[:t] == 'documentos'
-        @albums = @albums.select { _1.documents.attached? }
+      case params[:t]
+      when 'documentos'
+        @albums = @gallery.albums.where(category: 'document', published: true).order(updated_at: :desc)
+        # @albums = @albums.select { _1.documents.attached? }
         add_breadcrumb 'Acervo (Documentos)', documentos_galleries_path
         add_breadcrumb "#{@gallery.name} (Documentos)", gallery_path(@gallery), current: true
-      end
-      if params[:t] == 'imagens'
-        @albums = @albums.reject { _1.documents.attached? }
+      when 'imagens'
+        @albums = @gallery.albums.where(category: 'photo', published: true).order(updated_at: :desc)
+        # @albums = @albums.select { _1.documents.attached? }
         add_breadcrumb 'Acervo (Imagens)', imagens_galleries_path
         add_breadcrumb "#{@gallery.name} (Imagens)", gallery_path(@gallery), current: true
+      when 'videos'
+        @albums = @gallery.albums.where(category: 'video', published: true).order(updated_at: :desc)
+        # @albums = @albums.select { _1.documents.attached? }
+        add_breadcrumb 'Acervo (Videos)', videos_galleries_path
+        add_breadcrumb "#{@gallery.name} (Videos)", gallery_path(@gallery), current: true
       end
     end
     authorize @gallery
   end
 
   def documentos
-    # @galleries = policy_scope(Gallery)
-    #              .includes(albums: [:documents_attachments])
-    #              .with_published_albums
-    #              .order(name: :asc)
-    #              .uniq
-    #              .select(&:published_albums)
-    # @galleries = @galleries.select { |gallery| gallery.albums.any? { _1.documents.attached? } }
-    @galleries = policy_scope(Album).published_with_documents.map(&:gallery).uniq
+    @galleries = policy_scope(Album)
+                 .published_with_documents
+                 .map(&:gallery)
+                 .uniq
     add_breadcrumb 'Acervo', galleries_path, current: true
   end
 
   def imagens
-    @galleries = policy_scope(Gallery)
-                 .includes(albums: [:documents_attachments])
-                 .with_published_albums
-                 .order(name: :asc)
+    @galleries = policy_scope(Album)
+                 .published_with_photos
+                 .map(&:gallery)
                  .uniq
-                 .select(&:published_albums)
-    @galleries = @galleries.reject { |gallery| gallery.albums.all? { _1.documents.attached? } }
+    add_breadcrumb 'Acervo', galleries_path, current: true
+  end
 
+  def videos
+    @galleries = policy_scope(Album)
+                 .published_with_videos
+                 .map(&:gallery)
+                 .uniq
     add_breadcrumb 'Acervo', galleries_path, current: true
   end
 end
