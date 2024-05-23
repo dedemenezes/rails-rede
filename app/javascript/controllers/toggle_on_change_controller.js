fd2b22581229e301fdabe822c8912e06601f7753
Andre Menezes <djdedemenezes@gmail.com>
2024-05-23 18:33:06 -0300

    refactor attachment album form into partial and js behavior based on category

--- app/javascript/controllers/toggle_on_change_controller.js
+++ app/javascript/controllers/toggle_on_change_controller.js
@@ -2,19 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-on-change"
 export default class extends Controller {
-  static targets = ['video', 'photo', 'document']
+  static targets = ['category', 'video', 'photo', 'document']
   connect() {
-    console.log(this);
-    console.log(this.targets);
+    const elements = ['video', 'photo', 'document'].map(e => this[`${e}Target`])
+    console.log(elements);
+
+    console.log(this.categoryTarget);
+    console.log(this.categoryTarget.value);
+    console.log(this[`${this.categoryTarget.value}Target`]);
+    this.dynamicTarget(this.categoryTarget.value).classList.remove('d-none')
   }
 
   update(event) {
     ['video', 'photo', 'document'].forEach((name) => {
-      this[`${name}Target`].classList.add('d-none')
+      this.dynamicTarget(name).classList.add('d-none')
     })
     const selected = event.currentTarget.value
     if (selected !== '') {
-      this[`${selected}Target`].classList.remove('d-none')
+      this.dynamicTarget(selected).classList.remove('d-none')
     }
+  }
+
+  dynamicTarget(name) {
+    return this[`${name}Target`]
   }
 }
