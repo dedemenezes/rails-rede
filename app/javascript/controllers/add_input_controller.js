b3a1789d0ba7ba8cf859c14398877b224eabb3b8
Andre Menezes <djdedemenezes@gmail.com>
2024-05-24 12:31:37 -0300

    refac add input stimuus js to work when there are no inputs

--- app/javascript/controllers/add_input_controller.js
+++ app/javascript/controllers/add_input_controller.js
@@ -6,15 +6,29 @@ export default class extends Controller {
   connect() {
   }
 
-  new() {
-    this.nextInputIndex = this.inputTargets.length
-    const labelClone = this.inputTarget.previousElementSibling.cloneNode(true)
-    const labelCloneForValue = `album_videos_attributes_${this.nextInputIndex}_url`
-    labelClone.setAttribute('for', labelCloneForValue)
+  new(event) {
+    const newInputBlock = this.buildUrlFormInputBlock()
+    this.element.lastElementChild.insertAdjacentElement('beforebegin', newInputBlock)
+  }
 
+  labelHTML() {
+    // `<label class="form-label url required" for="album_videos_attributes_1_url">
+    //   Url
+    //   <abbr title="required">*</abbr>
+    // </label>`
+    const label = document.createElement('label')
+    label.classList.add('form-label')
+    label.classList.add('url')
+    label.classList.add('required')
+    label.setAttribute('for', `album_videos_attributes_${this.inputTargets.length}_url`)
+    label.innerHTML = `Url<abbr title="required">*</abbr>`
+    return label
+  }
+
+  inputHTML() {
     const inputElement = document.createElement('input')
     inputElement.type = 'url'
-    const name = `album[videos_attributes][${this.nextInputIndex}][url]`
+    const name = `album[videos_attributes][${this.inputTargets.length}][url]`
     inputElement.name = name
     inputElement.setAttribute('id', name)
     inputElement.dataset.addInputTarget = 'input'
@@ -22,11 +36,25 @@ export default class extends Controller {
     inputElement.classList.add('string')
     inputElement.classList.add('url')
     inputElement.classList.add('required')
+    return inputElement
+  }
 
-    const wraper = this.inputTarget.parentElement.cloneNode()
-    wraper.insertAdjacentElement('afterbegin', labelClone)
-    wraper.insertAdjacentElement('beforeend', inputElement)
+  wrapperHTML() {
+    // <div data-add-input-target="wrapper" class="mb-3 url required album_videos_url">
+    // </div>
+    const wrapper = document.createElement('div')
+    wrapper.classList.add('mb-3')
+    wrapper.classList.add('url')
+    wrapper.classList.add('required')
+    wrapper.classList.add('album_videos_url')
+    wrapper.dataset.addInputTarget = 'wrapper'
+    return wrapper
+  }
 
-    this.inputTargets[this.nextInputIndex - 1].parentElement.insertAdjacentElement('afterend', wraper)
+  buildUrlFormInputBlock() {
+    const formInputBlock = this.wrapperHTML()
+    formInputBlock.insertAdjacentElement('afterbegin', this.labelHTML(this.inputTargets.length))
+    formInputBlock.insertAdjacentElement('beforeend', this.inputHTML(this.inputTargets.length))
+    return formInputBlock
   }
 }
