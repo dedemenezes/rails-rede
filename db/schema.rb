1eda0b533886b584db0ced5f4ab02d89ba98bc82
Andre Menezes <djdedemenezes@gmail.com>
2024-06-03 12:38:59 -0300

    add description to videogst

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_29_234933) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_03_140407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -264,6 +264,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_29_234933) do
     t.datetime "updated_at", null: false
     t.boolean "published", default: false, null: false
     t.string "name"
+    t.string "description"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
