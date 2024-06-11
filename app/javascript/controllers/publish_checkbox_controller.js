20b0dfbdc6797c16cd8b1a2562b90e4303d98532
Andre Menezes <djdedemenezes@gmail.com>
2024-06-11 15:55:24 -0300

    refac simpler form config to display text input as design system

--- app/javascript/controllers/publish_checkbox_controller.js
+++ app/javascript/controllers/publish_checkbox_controller.js
@@ -3,15 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="publish-checkbox"
 export default class extends Controller {
   static targets = ['label', 'input']
+  static values = {
+    text: String
+  }
+
   connect() {
     this.updateHint()
   }
 
   updateHint(event = null) {
     if (this.inputTarget.checked) {
-      this.labelTarget.innerText = "Publicar ✅"
+      this.labelTarget.innerText = `${this.textValue} ✅`
     } else {
-      this.labelTarget.innerText = "Publicar ❌"
+      this.labelTarget.innerText = `${this.textValue} ❌`
     }
   }
 }
