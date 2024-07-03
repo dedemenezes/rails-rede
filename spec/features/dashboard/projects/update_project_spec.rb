8650318db84ea97bd353f7a256e73528ad278b2e
Andre Menezes <djdedemenezes@gmail.com>
2024-07-03 20:11:52 -0300

    fix

--- spec/features/dashboard/projects/update_project_spec.rb
+++ spec/features/dashboard/projects/update_project_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'Updating an existing project from dashboard', js: true do
+RSpec.describe 'Updating an existing project from dashboard', type: :feature do
   it 'updates the project and redirects back to dashboard project list' do
     create(:project)
     user = create(:coppola)
-    visit '/users/sign_in'
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: user.password
-    click_button 'Log in'
-    expect(page).to have_content 'Login efetuado com sucesso!'
+    login_as user
 
     visit "dashboard/projects/#{Project.last.id}/edit"
     expect(page).to have_content 'Projeto'
--- spec/features/visit_articles_spec.rb
+++ spec/features/visit_articles_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'Visiting articles index', js: true do
+RSpec.describe 'Visiting articles index', type: :feature do
   describe 'GET /articles' do
     it 'sees content' do
       visit articles_url
-      save_screenshot
+      # save_screenshot
       expect(page).to have_content('Notícias')
     end
   end
