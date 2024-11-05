3a043f21cc57b88b8e2279919107bc9e10d885b0
Andre Menezes <djdedemenezes@gmail.com>
2024-11-05 20:03:01 -0300

    REFAC - Remove Category route/controller/view for customer and admin

--- app/controllers/categories_controller.rb
+++
@@ -1,7 +0,0 @@
-class CategoriesController < ApplicationController
-  def show
-    @category = Category.find(params[:id])
-    @category_observatories = @category.observatories
-    authorize @category
-  end
-end
