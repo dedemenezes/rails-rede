d660159ecfc95a1b199ceeaf620b962174d77e81
Andre Menezes <djdedemenezes@gmail.com>
2024-11-05 19:59:03 -0300

    REFAC - Remove Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_05_223049) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_05_225423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,12 +78,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_05_223049) do
     t.index ["methodology_id"], name: "index_articles_on_methodology_id"
     t.index ["observatory_id"], name: "index_articles_on_observatory_id"
     t.index ["project_id"], name: "index_articles_on_project_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "conflict_types", force: :cascade do |t|
