4630fc166baa3dddc532e80cfb3d3fda5d8fe19f
Andre Menezes <djdedemenezes@gmail.com>
2024-11-06 18:59:17 -0300

    fix mapboxuploader test

--- test/services/mapbox_uploader_test.rb
+++ test/services/mapbox_uploader_test.rb
@@ -4,7 +4,7 @@ class MapboxUploaderTest < ActiveSupport::TestCase
   test '#ensure_valid_name' do
     name = 'São Francisco de Itabapoana'
     subject = MapboxUploader.new(tileset_name: name, file_path: "#{name}_path")
-    assert_equal 'são_francisco_de_itabapoana', subject.tileset_name
+    assert_equal 'sao_francisco_de_itabapoana', subject.tileset_name
 
   end
 end
