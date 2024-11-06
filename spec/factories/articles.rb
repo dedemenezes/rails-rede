0b1b52fcca7296cca1ba55c64f30d2b5e7586b85
Andre Menezes <djdedemenezes@gmail.com>
2024-11-05 22:35:29 -0300

    REFAC - add test for article methods

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -29,4 +29,20 @@ FactoryBot.define do
       )
     end
   end
+
+  factory :observatory_article_featured, class: 'Article' do
+    header { "FEATURED This is a very nice header for this article" }
+    sub_header { "FEATURED This is what we meant with a sub header" }
+    featured { true }
+    published { true }
+    association :observatory, factory: :ninho_do_urubu
+    rich_body do
+      ActionText::RichText.create(
+        { record_type: 'Article',
+          name: "content",
+          body: "<div class='trix-content'>FEATURED HELLO</div>",
+          record_id: id }
+      )
+    end
+  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,4 +9,23 @@ RSpec.describe Article, type: :model do
   describe 'Associations' do
     it { should have_one_attached(:banner) }
   end
+
+  describe '#featured?' do
+    it 'returns the right emoji' do
+      article = build(:article)
+      expect(article.featured?).to eq('❌')
+      article.featured = true
+      expect(article.featured?).to eq('✅')
+    end
+  end
+
+  describe '#observatory_name' do
+    it 'returns the observatory name' do
+      article = build(:observatory_article_featured)
+      expect(article.observatory_name).to eq('Ninho do Urubu')
+        article.observatory = nil
+        expect(article.observatory_name).to eq(nil)
+    end
+  end
+
 end
