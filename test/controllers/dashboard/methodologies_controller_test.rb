63d48ccacd1bb8f0bf53454e6d418bc4d178c867
Andre Menezes <djdedemenezes@gmail.com>
2024-11-06 18:59:45 -0300

    fix controller test path helper

--- test/controllers/dashboard/methodologies_controller_test.rb
+++ test/controllers/dashboard/methodologies_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Dashboard::MethodologiesControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get dashboard_methodologies_new_url
+    get new_dashboard_methodology_url
     assert_response :success
   end
 end
