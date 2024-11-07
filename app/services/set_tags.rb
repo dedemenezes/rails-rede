aae54ada1c1e6eecfbf385b1d7bbbdf31bd3714e
Andre Menezes <djdedemenezes@gmail.com>
2024-11-06 22:23:27 -0300

    add articles create as admin system test

--- app/services/set_tags.rb
+++ app/services/set_tags.rb
@@ -3,7 +3,7 @@ module SetTags
     model_name_as_symbol = extract_model_name(instance)
     return unless params[model_name_as_symbol][:tag_ids].present?
 
-    ids = sanitize_and_remove_blanks(params)
+    ids = sanitize_and_remove_blanks(params[model_name_as_symbol][:tag_ids])
     # tag_names = params[model_name_as_symbol][:tag_ids].select { |id| id.match?(/[a-zA-Z]/) }
     # ids.delete('')
     # removed_taggings = instance.taggings.reject { |tagging| ids.include? tagging.tag.id.to_s }
@@ -22,7 +22,7 @@ module SetTags
     instance.model_name.to_s.downcase.parameterize.gsub('-', '_').to_sym
   end
 
-  def self.sanitize_and_remove_blanks(params)
-    params[model_name_as_symbol][:tag_ids].grep_v(/[a-zA-Z]/).compact_blank
+  def self.sanitize_and_remove_blanks(ids)
+    ids.grep_v(/[a-zA-Z]/).compact_blank
   end
 end
