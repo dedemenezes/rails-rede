665150c15334f72de2aacbc09f88bd78d6ac4edd
Andre Menezes <djdedemenezes@gmail.com>
2024-11-07 10:07:05 -0300

    add project create integration test

--- test/controllers/dashboard/projects_controller_test.rb
+++ test/controllers/dashboard/projects_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class Dashboard::ProjectsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users(:coppola)
+  end
+
+  test 'should create a project' do
+    assert_difference('Project.count', 1) do
+      post dashboard_projects_path, params: {
+          project: {
+            name: 'Test Project',
+            banner_text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit'
+          }
+        }
+    end
+    assert_response :redirect
+    assert_redirected_to dashboard_projects_path
+  end
 end
