0fca70554e2bac721cb63cf7b48c0fa878dea429
Andre Menezes <djdedemenezes@gmail.com>
2024-12-10 19:28:42 -0300

    add remove video input action js

--- app/javascript/controllers/duplicate_inputs_controller.js
+++ app/javascript/controllers/duplicate_inputs_controller.js
@@ -1,34 +1,52 @@
 import { Controller } from "@hotwired/stimulus"
-import { controllers } from "trix";
-import { ContextExclusionPlugin } from "webpack";
 
 // Connects to data-controller="duplicate-inputs"
 export default class extends Controller {
-  static targets = ['formFields', 'title']
+  static targets = ['formFields', 'title', 'removeBtn']
   connect() {
     console.log("connected!");
+  }
+
+  remove() {
+    if (this.formFieldsTargets.length === 1) {
+      return
+    }
+
+    const lastVideoInputs = this.formFieldsTargets.pop()
+    lastVideoInputs.remove()
+    if (this.formFieldsTargets.length === 1) {
+      this.removeBtnTarget.classList.add('d-none')
+    }
   }
 
   add() {
     const clone = this.formFieldsTarget.cloneNode(true)
     const inputNumber = this.element.childElementCount - 1
 
+    // rename inputs accordingly
     clone.querySelectorAll('input')
-    .forEach((label) => {
-      label.setAttribute('id', label.id.replace(/\d/, inputNumber))
-      label.setAttribute('name', label.name.replace(/\d/, inputNumber))
+    .forEach((input) => {
+      input.setAttribute('id', input.id.replace(/\d/, inputNumber))
+      input.setAttribute('name', input.name.replace(/\d/, inputNumber))
     })
 
+    // rename labels accordingly
     clone.querySelectorAll('label')
     .forEach((label) => {
       label.setAttribute('for', label.getAttribute('for').replace(/\d/, inputNumber))
     })
 
+    // rename title accordingly
     const cloneTitle = clone.querySelector('p')
-    console.log(cloneTitle)
     cloneTitle.innerText = cloneTitle.innerText.replace(/\d/, inputNumber + 1)
 
+    // insert before video input actions
     this.element.insertBefore(clone, this.element.lastElementChild)
+
+    if (this.formFieldsTargets.length === 2) {
+      this.removeBtnTarget.classList.remove('d-none')
+    }
+
   }
 }
 
