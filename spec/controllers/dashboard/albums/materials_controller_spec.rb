ae7971cc014b93bca41b28ebca4346dcc557c865
Andre Menezes <djdedemenezes@gmail.com>
2024-12-18 15:15:46 -0300

    remove empty line, devise test controller helpers and empty test file

--- spec/controllers/dashboard/albums/materials_controller_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Dashboard::Albums::MaterialsController, type: :controller do
-end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -38,7 +37,6 @@ Capybara.javascript_driver = :selenium_chrome_headless
 
 RSpec.configure do |config|
   config.include Warden::Test::Helpers
-  # config.include Devise::Test::ControllerHelpers
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{Rails.root}/spec/fixtures"
 
