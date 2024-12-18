da3744287fe1dfbc67a5cc6e7cc9cd49af7531ee
Andre Menezes <djdedemenezes@gmail.com>
2024-12-18 15:00:52 -0300

    skip default breadcrumb on materials controller

--- app/controllers/dashboard/albums/materials_controller.rb
+++ app/controllers/dashboard/albums/materials_controller.rb
@@ -1,4 +1,5 @@
 class Dashboard::Albums::MaterialsController < DashboardController
+  skip_before_action :add_default_dashboard_breadcrumb
   def index
     # get all albums with videos and documents
     documents = Album.includes(:gallery, banner_attachment: :blob).with_documents
