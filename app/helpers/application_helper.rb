c1260a80987fbffd72e12d5bbc31e0e6eb8e94bc
Andre Menezes <djdedemenezes@gmail.com>
2025-05-26 14:28:31 -0300

    refac banner image to work correctly on development

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,20 +26,28 @@ module ApplicationHelper
   end
 
   def display_banner_as_background(instance)
-    if instance&.banner&.attached?
-      "https://rede-observacao-prod.s3.us-east-2.amazonaws.com/#{instance.banner.key}"
+    if Rails.env.development?
+      cl_image_path(instance.banner.key)
     else
-      image_path('default-banner.png')
+      if instance&.banner&.attached?
+        "https://rede-observacao-prod.s3.us-east-2.amazonaws.com/#{instance.banner.key}"
+      else
+        image_path('default-banner.png')
+      end
     end
   end
 
   def display_banner_image(photo, options = {})
-    if photo.attached?
-      url = "https://rede-observacao-prod.s3.us-east-2.amazonaws.com/#{photo.key}"
+    if Rails.env.development?
+      cl_image_tag(photo.key, width: options[:width], style: options[:style])
     else
-      url = 'default-banner.png'
+      if photo.attached?
+        url = "https://rede-observacao-prod.s3.us-east-2.amazonaws.com/#{photo.key}"
+      else
+        url = 'default-banner.png'
+      end
+      image_tag(url, options)
     end
-    image_tag(url, options)
   end
 
   def display_banner_as_background_image(photo, options = {})
