121f1c610a91f1adb9c9c63351ba86e48d257100
Andre Menezes <djdedemenezes@gmail.com>
2025-05-26 14:51:16 -0300

    remove unecessary tests

--- test/controllers/galleries_controller_test.rb
+++ test/controllers/galleries_controller_test.rb
@@ -11,20 +11,4 @@ class GalleriesControllerTest < ActionDispatch::IntegrationTest
 
     assert_select "meta[property='og:title'][content='#{@gallery.name} - Rede Observação']"
   end
-
-  test 'should GET acervos/documentos' do
-    get documentos_galleries_path
-    assert_response :success
-    assert_select 'span', with: '(Documentos)'
-  end
-
-  test 'documents index show only published albums' do
-    get documentos_galleries_path
-    assert_select '.observatory__card', count: 1
-  end
-
-  test "documents index show link to album" do
-    get documentos_galleries_path
-    assert_select 'a', href: "/acervos/#{@gallery.name}?t=documentos"
-  end
 end
