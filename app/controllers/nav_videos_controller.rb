7694e28f6eaf983bde1da08cf53b5dee6159d374
Andre Menezes <djdedemenezes@gmail.com>
2025-07-15 13:30:00 -0300

    refac: remove instance variable from partial

--- app/controllers/nav_videos_controller.rb
+++ app/controllers/nav_videos_controller.rb
@@ -1,6 +1,6 @@
 class NavVideosController < ApplicationController
   def index
     @nav_videos = policy_scope(Video).order(created_at: :desc).limit(2)
-    render partial: "shared/nav_videos"
+    render partial: "shared/nav_videos", locals: { nav_videos: @nav_videos }
   end
 end
