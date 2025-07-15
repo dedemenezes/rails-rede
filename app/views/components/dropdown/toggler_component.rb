d580f4f5367b8452998671930a0a64e797374bef
Andre Menezes <djdedemenezes@gmail.com>
2025-07-15 15:49:50 -0300

    refac dropdown toggle to close only when clicking outside

--- app/views/components/dropdown/toggler_component.rb
+++ app/views/components/dropdown/toggler_component.rb
@@ -6,7 +6,7 @@ class Dropdown::TogglerComponent < ApplicationComponent
   end
 
   def view_template
-    button(class: 'nav-link', type: 'button', data: { bs_toggle: "dropdown" }, aria_expanded: "false") do
+    button(class: 'nav-link', type: 'button', data: { bs_auto_close: "outside", bs_toggle: "dropdown" }, aria_expanded: "false") do
       span(class: "navbar-menu-item-arrow") { text }
       i(class: "fa-solid fa-chevron-down", style: "opacity: 0.7;")
     end
