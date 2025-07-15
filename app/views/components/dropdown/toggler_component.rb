beae29129b706848c29284b72efdfc75bb2a6f04
Andre Menezes <djdedemenezes@gmail.com>
2025-07-15 12:39:17 -0300

    add new dropdown toggler component

---
+++ app/views/components/dropdown/toggler_component.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Dropdown::TogglerComponent < ApplicationComponent
+  def initialize(text)
+    @text = text
+  end
+
+  def view_template
+    button(class: 'nav-link', type: 'button', data: { bs_toggle: "dropdown" }, aria_expanded: "false") do
+      span(class: "navbar-menu-item-arrow") { text }
+      i(class: "fa-solid fa-chevron-down", style: "opacity: 0.7;")
+    end
+  end
+
+  attr_reader :text
+end
