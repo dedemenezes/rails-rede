5e3d15970931b811844b2d531972d624c7a734fa
Andre Menezes <djdedemenezes@gmail.com>
2025-07-15 12:39:43 -0300

    refac nav link component so we can defaulkt active class

--- app/views/components/nav_link_component.rb
+++ app/views/components/nav_link_component.rb
@@ -4,18 +4,32 @@ class NavLinkComponent < ApplicationComponent
   include Phlex::Rails::Helpers::LinkTo
   include Phlex::Rails::Helpers::Request
 
-  def initialize(text, link_path, opts={})
-    @link_path = link_path
+  def initialize(text, link_path, active_class: nil, css_class: nil)
     @text = text
-    @css_class = opts[:css_class]
+    @link_path = link_path
+    @css_class = css_class
+    @active_class = active_class
   end
 
   def view_template
-    link_to link_path, class: "nav-link  #{current_class?(link_path)} #{css_class}" do
+    link_to link_path, class: final_class do
       text
     end
   end
 
   private
-  attr_reader :link_path, :text, :css_class
+
+  attr_reader :text, :link_path, :css_class, :active_class
+
+  def final_class
+    [
+      "nav-link",
+      active_class || auto_active_class,
+      css_class
+    ].compact.join(" ")
+  end
+
+  def auto_active_class
+    request.path == link_path ? "active" : nil
+  end
 end
