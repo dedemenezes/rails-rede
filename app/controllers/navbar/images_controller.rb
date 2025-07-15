282731537b96b27d40b3ebc4ea55dfc233bda073
Andre Menezes <djdedemenezes@gmail.com>
2025-07-15 16:30:12 -0300

    wip: cascading navigation for Acervos into the navbar

--- app/controllers/navbar/images_controller.rb
+++ app/controllers/navbar/images_controller.rb
@@ -1,13 +1,10 @@
 class Navbar::ImagesController < ApplicationController
   def index
-    binding.b
     @galleries = policy_scope(Album)
               .published_with_photos
               .limit(3)
               .map(&:gallery)
               .uniq
-    # p @galleries
-    # binding.break
     render partial: "shared/nav_images", locals: { nav_albums: @galleries }
   end
 end
