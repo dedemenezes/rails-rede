d011a3be28a2789f7f4c595ae412b852af8b47c3
Andre Menezes <djdedemenezes@gmail.com>
2025-07-16 14:51:54 -0300

    rename variables to mathc instances and remove collection limitation

--- app/controllers/navbar/images_controller.rb
+++ app/controllers/navbar/images_controller.rb
@@ -2,9 +2,8 @@ class Navbar::ImagesController < ApplicationController
   def index
     @galleries = policy_scope(Album)
               .published_with_photos
-              .limit(3)
               .map(&:gallery)
               .uniq
-    render partial: "shared/nav_images", locals: { nav_albums: @galleries }
+    render partial: "shared/nav_images", locals: { nav_galleries: @galleries }
   end
 end
