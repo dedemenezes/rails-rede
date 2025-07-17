37259431747db08d10b2ce1d6c27cf45749538e3
Andre Menezes <djdedemenezes@gmail.com>
2025-07-17 10:42:50 -0300

    refac to follow same structur into whole navbar

--- config/routes/app.rb
+++ config/routes/app.rb
@@ -30,9 +30,9 @@ namespace :articles do
   end
 end
 
-get 'nav_videos', to: 'nav_videos#index', as: :nav_videos
 
 namespace :navbar do
+  get 'videos', to: 'videos#index', as: :videos
   get 'images', to: 'images#index', as: :images
   get 'materials', to: 'materials#index', as: :materials
   get 'gallery-show/:name', to: 'galleries#show', as: :gallery
