be583542d4f109393f6845292f792c77700e0556
Andre Menezes <djdedemenezes@gmail.com>
2025-07-17 13:50:29 -0300

    wip: improve tab component to be easily accessible in system tests and start navbar system test

---
+++ test/system/navbar_navigations_test.rb
@@ -0,0 +1,16 @@
+require "application_system_test_case"
+
+class NavbarNavigationsTest < ApplicationSystemTestCase
+  test "Navigating through the navbar" do
+    observatory = observatories(:ninho_do_urubu)
+    articles(:one_not_featured).update(main_featured: true)
+
+    visit root_path
+
+    assert_selector "h1", text: "osfhdsf sdofhsdfjsofh fsd fosd f isdhf jsfn kdjsdfs asdad dasd ass"
+    assert_selector "button", id: "observatory-dropdown-toggler"
+
+    click_button("observatory-dropdown-toggler")
+    assert_selector "div.dropdown-menu.show"
+  end
+end
