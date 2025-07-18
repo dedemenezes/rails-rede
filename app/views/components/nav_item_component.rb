173a9c974ba366fe98188ce33e0cd16612105782
Andre Menezes <djdedemenezes@gmail.com>
2025-07-18 10:15:58 -0300

    feat: nav-item component can take extra css classes

--- app/views/components/nav_item_component.rb
+++ app/views/components/nav_item_component.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 class NavItemComponent < ApplicationComponent
+  def initialize(options={})
+    @extra_css = options[:extra_css] || ""
+  end
+
   def view_template
-    li(class: "nav-item") do
+    li(class: "nav-item #{extra_css}") do
       yield
     end
   end
+
+  private
+
+  attr_reader :extra_css
 end
