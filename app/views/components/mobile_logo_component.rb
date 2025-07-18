e1873275a50877ec9ac0cb1c1a18aa1b806b9ff0
Andre Menezes <djdedemenezes@gmail.com>
2025-07-18 16:38:13 -0300

    add mobile logo component and mobileNavItem component

---
+++ app/views/components/mobile_logo_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class MobileLogoComponent < ApplicationComponent
+  include Phlex::Rails::Helpers::LinkTo
+  include Phlex::Rails::Helpers::ImageTag
+
+  def view_template
+    link_to root_path, class: "navabar-top-logo offcanvas-title", id: "menuMobileLabel" do
+        image_tag 'Logo_PEA.png', width: '186px', alt: 'Logo do PEA Rede Observação'
+    end
+  end
+end
