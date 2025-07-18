ad2067b62bc9a9544a3d05e83f13a877599ca86d
Andre Menezes <djdedemenezes@gmail.com>
2025-07-18 10:14:33 -0300

    refac: dropdown tab component loads with spinner before displaying content

--- app/views/components/dropdown/tab_content_component.rb
+++ app/views/components/dropdown/tab_content_component.rb
@@ -2,8 +2,7 @@
 
 class Dropdown::TabContentComponent < ApplicationComponent
   include Phlex::Rails::Helpers::TurboFrameTag
-  def initialize(text, url, options={})
-    @text = text
+  def initialize(url, options={})
     @url = url
     @frame_id = options[:frame_id]
     @aria_labelledby = options[:aria_labelledby]
@@ -13,8 +12,9 @@ class Dropdown::TabContentComponent < ApplicationComponent
 
   def view_template
     turbo_frame_tag(frame_id, class: "tab-pane #{'active' if active?}" , role: "tabpanel", aria_labelledby: aria_labelledby, tabindex: index, src: url, loading: "lazy") do
-      div(class: "text-muted") do
-        plain text + frame_id
+      div(style: "position: absolute; top: 40%; left: 50%") do
+        # loads always with spinner
+        div(class: "spinner-border text-primary", role: "status")
       end
     end
   end
@@ -24,5 +24,5 @@ class Dropdown::TabContentComponent < ApplicationComponent
   def active?
     @active == true
   end
-  attr_reader :text, :url, :frame_id, :aria_labelledby, :index
+  attr_reader :url, :frame_id, :aria_labelledby, :index
 end
