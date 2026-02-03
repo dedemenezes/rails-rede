b9e6fae7e1f7813820e6cff3356bd91c043a0606
Andre Menezes <djdedemenezes@gmail.com>
2026-02-02 21:34:35 -0300

    remove unfinished test

--- test/system/navbar_navigations_test.rb
+++ test/system/navbar_navigations_test.rb
@@ -1,21 +1,24 @@
 require "application_system_test_case"
 
 class NavbarNavigationsTest < ApplicationSystemTestCase
-  test "Navigating through the navbar" do
-    observatory = observatories(:ninho_do_urubu)
-    articles(:one_not_featured).update(main_featured: true)
+  # setup do
+  #   albums(:published_with_one_photo)
+  #   observatory = observatories(:ninho_do_urubu)
+  #   articles(:one_not_featured).update(main_featured: true)
+  # end
+  # test "Navigating through the navbar" do
 
-    visit root_path
+  #   visit root_path
 
-    assert_selector "h1", text: "osfhdsf sdofhsdfjsofh fsd fosd f isdhf jsfn kdjsdfs asdad dasd ass"
-    assert_selector "button", id: "observatory-dropdown-toggler"
+  #   assert_selector "h1", text: "osfhdsf sdofhsdfjsofh fsd fosd f isdhf jsfn kdjsdfs asdad dasd ass"
+  #   assert_selector "button", id: "observatory-dropdown-toggler"
 
-    click_button("observatory-dropdown-toggler")
-    assert_selector "div.dropdown-menu.show"
-    assert_link "Ninho do Urubu"
+  #   click_button("observatory-dropdown-toggler")
+  #   assert_selector "div.dropdown-menu.show"
+  #   assert_link "Ninho do Urubu"
 
-    click_button("acervos-dropdown-toggler")
-    assert_selector "div.dropdown-menu.show"
-    assert_link "Ninho do Urubu"
-  end
+  #   click_button("acervos-dropdown-toggler")
+  #   assert_selector "div.dropdown-menu.show"
+  #   assert_link "Ninho do Urubu"
+  # end
 end
