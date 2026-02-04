2bcfe7cdc535ceef92c9692e7f9a6025a019b9b0
Andre Menezes <djdedemenezes@gmail.com>
2026-02-04 00:18:25 -0300

    fix autoload

--- app/models/album/documentable.rb
+++ app/models/album/documentable.rb
@@ -4,6 +4,7 @@ module Album
 
     included do
       has_many_attached :documents
+
       scope :with_documents, -> { left_joins(:documents_attachments).where(category: 'document').group(:id) }
       scope :published_with_documents, lambda {
         with_documents.where(published: true).having("COUNT(active_storage_attachments) > 0")
