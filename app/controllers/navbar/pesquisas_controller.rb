0d9c2fda12dc9e5bd67fcb77d794ee398905e631
Andre Menezes <djdedemenezes@gmail.com>
2026-02-07 13:06:46 -0300

    remove filter for formacao e pesquisa for produto navbar pesquisa

--- app/controllers/navbar/pesquisas_controller.rb
+++ app/controllers/navbar/pesquisas_controller.rb
@@ -3,8 +3,7 @@ module Navbar
     def index
       @galleries = policy_scope(Gallery).joins(albums: :documents_attachments)
                                         .where.not(methodology_id: nil)
-                                        .where(name: "Formação e Pesquisa", albums: { published: true,
-                                                                                      category: 'document' })
+                                        .where(albums: { published: true, category: 'document' })
                                         .distinct
                                         .order(name: :asc)
       if params[:q].present?
